9e4e58bd0180c3645b8427044ca6bd29781424a3
Jason <jwrennie@gmail.com>
2026-05-21 23:12:39 +0000

    chore: remove stray temp files

--- tmpe1jg1t9y/nats_pub.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import nats
-
-async def main():
-    nc = await nats.connect("nats://nats:4222")
-    await nc.publish("council.broadcast", b"hello-e2e")
-    await nc.close()
-
-asyncio.run(main())
